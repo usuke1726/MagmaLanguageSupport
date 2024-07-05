d0531a389c154193705b6c477fbf8c77a1a42eed
usuke1726 <78670588+usuke1726@users.noreply.github.com>
2024-07-05 15:05:30 +0900

    `load` 文の行末にコメントがある場合に読み込まれない不具合を修正

--- src/FileHandler.ts
+++ src/FileHandler.ts
@@ -308,8 +308,8 @@ export default class FileHandler{
         scope = "global";
         const parser = new DocumentParser(uri);
         const diagnostics: vscode.Diagnostic[] = [];
-        const loadStatementWithAtMark = /^(\s*load\s+")(@.+?)";\s*$/;
-        const requireComment = /^(\s*\/\/\s+@requires?\s+")(.+?)";?\s*$/;
+        const loadStatementWithAtMark = /^(\s*load\s+")(@.+?)";\s*(\/\/.*)?$/;
+        const requireComment = /^(\s*\/\/\s+@requires?\s+")(.+?)";?/;
         const startComment = /^\s*\/\*\*(.*)$/;
         const inComment = /^\s*\*? {0,2}(.*)$/;
         const endComment = /^(.*)\*\/\s*$/;
